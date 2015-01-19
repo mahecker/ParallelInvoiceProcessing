aa03cfe1143f112d7232afb14641ee5b5e44cb46
Marlon Hecker <marlon.hecker@fh-muenster.de>
2015-01-19 10:04:41 +0100

    Adding Logger config-File "log4j2.xml"

--- InvoiceProcessing/src/de/fhm/DataProcessing/DocumentGenerator.java
+++ InvoiceProcessing/src/de/fhm/DataProcessing/DocumentGenerator.java
@@ -1,5 +1,17 @@
 package de.fhm.DataProcessing;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DocumentGenerator {
+
+  private static final Logger logger = LogManager.getLogger(DocumentGenerator.class);
+
+  public static void main(String[] args) {
+	
+	System.out.println("Test");
+	logger.trace("Entering main()-Method...");
+
+  }
 
 }
