1a532efa798e6e3143ef27c6b176677d47dafd1a
Marlon Hecker <marlon.hecker@fh-muenster.de>
2015-02-09 15:57:51 +0100

    XSL als Vorlage für Rechnungserstellung in PDF angefügt

--- InvoiceProcessing/src/de/fhm/DataOutput/InvoiceDataOutput.java
+++ InvoiceProcessing/src/de/fhm/DataOutput/InvoiceDataOutput.java
@@ -1,5 +1,47 @@
 package de.fhm.DataOutput;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+
 public class InvoiceDataOutput {
+  private static final Logger logger = LogManager.getLogger(InvoiceDataOutput.class);
+  
+  public void test(Document[] docs) {
+
+	for (int i = 0; i < docs.length; i++) {
+	  try {
+		printDocument(docs[i], System.out);
+	  } catch (IOException | TransformerException e) {
+		logger.error(e.getMessage());
+	  }
+	}
+
+  }
+
+  // Quelle:
+  // http://stackoverflow.com/questions/2325388/java-shortest-way-to-pretty-print-to-stdout-a-org-w3c-dom-document
+  private static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+	TransformerFactory tf = TransformerFactory.newInstance();
+	Transformer transformer = tf.newTransformer();
+	transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+	transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+	transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+  }
 
 }
--- InvoiceProcessing/src/de/fhm/DataProcessing/InvoiceDataProcessing.java
+++ InvoiceProcessing/src/de/fhm/DataProcessing/InvoiceDataProcessing.java
@@ -1,20 +1,10 @@
 package de.fhm.DataProcessing;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Document;
 
+import de.fhm.DataOutput.InvoiceDataOutput;
 import de.fhm.DataSource.InvoiceDataSource;
 
 public class InvoiceDataProcessing {
@@ -25,6 +15,7 @@ public class InvoiceDataProcessing {
 
   public static void main(String[] args) {
 	InvoiceDataSource source = new InvoiceDataSource();
+	InvoiceDataOutput output = new InvoiceDataOutput();
 	Document[] docs = null;
 	long start;
 
@@ -35,33 +26,11 @@ public class InvoiceDataProcessing {
 	logger.trace("/-> [START] Getting Data from Source...");
 	start = System.currentTimeMillis();
 	docs = source.generateData(start);
-
-	for (int i = 0; i < docs.length; i++) {
-	  try {
-		printDocument(docs[i], System.out);
-	  } catch (IOException | TransformerException e) {
-		logger.error(e.getMessage());
-	  }
-	}
+	output.test(docs);
 
 	logger.trace("-------------------------------------------");
 	logger.trace("### Ending Invoice Processing: " + (System.currentTimeMillis() - INITIALIZATION_TIME) + " ms. ###");
 	logger.trace("-------------------------------------------");
 
   }
-
-  // Quelle:
-  // http://stackoverflow.com/questions/2325388/java-shortest-way-to-pretty-print-to-stdout-a-org-w3c-dom-document
-  private static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
-	TransformerFactory tf = TransformerFactory.newInstance();
-	Transformer transformer = tf.newTransformer();
-	transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-	transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-	transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-	transformer.transform(new DOMSource(doc), new StreamResult(new OutputStreamWriter(out, "UTF-8")));
-  }
-
 }
